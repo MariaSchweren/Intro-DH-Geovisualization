1dbaf4bd5d978bbb79f2926de965465e9df3a02c
fz31dovo <fz31dovo@studserv.uni-leipzig.de>
2022-03-04 00:24:25 +0100

    Added info sidepanel to map

--- map.R
+++ map.R
@@ -17,20 +17,79 @@ demo1_lat = 51.010409
 demo1_lng = 16.29121
 
 df <- data.frame(lat, lng)
+csv <- read.csv(file="C:\\Users\\Fabian\\Documents\\data\\data.csv", encoding="UTF-8")
 
 ui <- fluidPage(
-  leafletOutput("map", width=800, height=800),
-  dateRangeInput("time", NULL, start="1945-01-01", end="1945-05-08", language="de", weekstart=1, width="500px"),
-  checkboxInput("osrm", "OSRM", FALSE),
-  actionButton("center", "Center")
+
+  sidebarLayout(position = "right",
+                sidebarPanel(h4("Name"),
+                             verbatimTextOutput("name"),
+                             h4("Ort"),
+                             verbatimTextOutput("ort"),
+                             h4("Betreiber"),
+                             verbatimTextOutput("betreiber"),
+                             h4("Dauer des Bestehens"),
+                             verbatimTextOutput("dauer"),
+                             h4("Haeftlingsbelegung"),
+                             verbatimTextOutput("belegung"),
+                             h4("Unterbringung"),
+                             verbatimTextOutput("unterbringung"),
+                             h4("Art der Arbeiten"),
+                             verbatimTextOutput("arbeiten"),
+                             h4("Todesopfer"),
+                             verbatimTextOutput("todesopfer"),
+                             h4("Rueckueberstellungen"),
+                             verbatimTextOutput("rueckueberstellungen"),
+                             h4("Fluchten"),
+                             verbatimTextOutput("fluchten"),
+                             h4("Zugaenge aus anderen Lagern"),
+                             verbatimTextOutput("zugaenge")),
+                mainPanel(leafletOutput("map", width=1200, height=800),
+                          dateRangeInput("time", NULL, start="1945-01-01", end="1945-05-08", language="de", weekstart=1, width="500px"),
+                          checkboxInput("osrm", "OSRM", FALSE),
+                          actionButton("center", "Center"),)
+  )
 )
+
+square_green <-
+  makeIcon(iconUrl = "http://www.clipartbest.com/cliparts/niE/yKR/niEyKRyoT.jpeg", iconWidth = 18, iconHeight = 18)
 
 server <- function(input, output, session) {
   output$map <- renderLeaflet(leaflet() %>% setView(lng=13.5, lat=50.95, zoom=8) %>% addTiles() %>% addMarkers(lat=demo1_lat, lng=demo1_lng))
   
   for(i in 1:length(a)) {
-    leafletProxy('map') %>% addMarkers(lat=a[[i]]$lat, lng=a[[i]]$lng) %>% addPolylines(lat=a[[i]]$lat, lng=a[[i]]$lng)
+    leafletProxy('map') %>% addMarkers(layerId=i, lat=a[[i]]$lat, lng=a[[i]]$lng, icon=square_green) %>% addPolylines(layerId=i, lat=a[[i]]$lat, lng=a[[i]]$lng, color="red")
   }
+  
+  observeEvent(input$map_marker_click, { 
+    p <- input$map_marker_click
+    output$name <- renderText({csv$Name[p$id]})
+    output$ort <- renderText({csv$Ort[p$id]})
+    output$betreiber <- renderText({csv$Betreiber[p$id]})
+    output$dauer <- renderText({csv$Dauer.des.Bestehens[p$id]})
+    output$belegung <- renderText({csv$H.ftlingsbelegung[p$id]})
+    output$unterbringung <- renderText({csv$Unterbringung[p$id]})
+    output$arbeiten <- renderText({csv$Art.der.Arbeiten[p$id]})
+    output$todesopfer <- renderText({csv$Todesopfer[p$id]})
+    output$rueckueberstellungen <- renderText({csv$R.ck.berstellungen[p$id]})
+    output$fluchten <- renderText({csv$Fluchten[p$id]})
+    output$zugaenge <- renderText({csv$Zug.nge.aus.anderen.Lagern[p$id]})
+  })
+  
+  observeEvent(input$map_shape_click, { 
+    p <- input$map_shape_click
+    output$name <- renderText({csv$Name[p$id]})
+    output$ort <- renderText({csv$Ort[p$id]})
+    output$betreiber <- renderText({csv$Betreiber[p$id]})
+    output$dauer <- renderText({csv$Dauer.des.Bestehens[p$id]})
+    output$belegung <- renderText({csv$H.ftlingsbelegung[p$id]})
+    output$unterbringung <- renderText({csv$Unterbringung[p$id]})
+    output$arbeiten <- renderText({csv$Art.der.Arbeiten[p$id]})
+    output$todesopfer <- renderText({csv$Todesopfer[p$id]})
+    output$rueckueberstellungen <- renderText({csv$R.ck.berstellungen[p$id]})
+    output$fluchten <- renderText({csv$Fluchten[p$id]})
+    output$zugaenge <- renderText({csv$Zug.nge.aus.anderen.Lagern[p$id]})
+  })
   
   observeEvent(input$time, {
     leafletProxy('map') %>% clearMarkers() %>% clearShapes()
