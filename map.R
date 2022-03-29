1894b9a4a498b48fa141bd51aeaa321a6aede1a0
fz31dovo <fz31dovo@studserv.uni-leipzig.de>
2022-03-29 14:31:36 +0200

    Added more routes and improved map

--- map.R
+++ map.R
@@ -2,6 +2,7 @@ library(shiny)
 library(leaflet)
 library(osrm)
 library(geojsonio)
+library(shinyjs)
 
 csv <- read.csv(file="C:\\Users\\Fabian\\Documents\\data\\data.csv", encoding="UTF-8")
 locations <- read.csv(file="C:\\Users\\Fabian\\Documents\\data\\locations_matched_osrm.csv", encoding="UTF-8")
@@ -15,34 +16,79 @@ for(i in locations[,1]) { # TODO increase id in csv by one
   entry <- list(lat=location_lat,lng=location_lng,osrm_lat=osrm_lat,osrm_lng=osrm_lng)
   data[[i+1]] <- entry 
 }
+type <- ""
 
 ui <- fluidPage(
+  shinyjs::useShinyjs(),
   sidebarLayout(position = "right",
-                sidebarPanel(h4("Name"),
-                             textOutput("name"),
-                             h4("Ort"),
-                             textOutput("ort"),
-                             h4("Betreiber"),
-                             textOutput("betreiber"),
-                             h4("Dauer des Bestehens"),
-                             textOutput("dauer"),
-                             h4("Haeftlingsbelegung"),
-                             textOutput("belegung"),
-                             h4("Unterbringung"),
-                             textOutput("unterbringung"),
-                             h4("Art der Arbeiten"),
-                             textOutput("arbeiten"),
-                             h4("Todesopfer"),
-                             textOutput("todesopfer"),
-                             h4("Rueckueberstellungen"),
-                             textOutput("rueckueberstellungen"),
-                             h4("Fluchten"),
-                             textOutput("fluchten"),
-                             h4("Zugaenge aus anderen Lagern"),
-                             textOutput("zugaenge")),
+                sidebarPanel(
+                  h4("Name"),
+                  textOutput("name"),
+                  h4("Ort"),
+                  textOutput("ort"),
+                  h4("Betreiber"),
+                  textOutput("betreiber"),
+                  div(id="aussenlager", 
+                      h4("Dauer des Bestehens"),
+                      textOutput("dauer"),
+                      h4("Haeftlingsbelegung"),
+                      textOutput("belegung"),
+                      h4("Unterbringung"),
+                      textOutput("unterbringung"),
+                      h4("Art der Arbeiten"),
+                      textOutput("arbeiten"),
+                      h4("Todesopfer"),
+                      textOutput("todesopfer"),
+                      h4("Rueckueberstellungen"),
+                      textOutput("rueckueberstellungen"),
+                      h4("Fluchten"),
+                      textOutput("fluchten"),
+                      h4("Zugaenge aus anderen Lagern"),
+                      textOutput("zugaenge"),
+                      h4("Evakuierung"),
+                      textOutput("evakuierung"),
+                      h4("Juristische Aufarbeitung"),
+                      textOutput("aufarbeitung"),
+                      h4("Besonderheiten der Evakuierung"),
+                      textOutput("evakuierung_besonderheiten"),
+                      h4("Besonderheiten des Lagers"),
+                      textOutput("lager_besonderheiten"),
+                      h4("Ende der Evakuierung"),
+                      textOutput("evakuierung_ende"),
+                      h4("Todesopfer/Vorkommnisse"),
+                      textOutput("vorkommnisse"),
+                      h4("verlauf/Orte"),
+                      textOutput("verlauf")),
+                  hidden(div(id="marsch", 
+                             h4("Starke der Kolonne"),
+                             textOutput("kolonne_marsch"),
+                             h4("Beginn der Evakuierung"),
+                             textOutput("evakuierung_beginn_marsch"),
+                             h4("Marsch auf saechsischem Gebiet"),
+                             textOutput("sachsen_marsch"),
+                             h4("weitere Evakuierung"),
+                             textOutput("evakuierung_weitere_marsch"),
+                             h4("Ende/Befreiung"),
+                             textOutput("ende_marsch"),
+                             h4("Besonderheiten"),
+                             textOutput("besonderheiten_marsch"),
+                             h4("Ende der Evakuierung"),
+                             textOutput("evakuierung_ende_marsch"),
+                             h4("Todesopfer/Vorkommnisse"),
+                             textOutput("vorkommnisse_marsch"),
+                             h4("Haeftlingstaerke"),
+                             textOutput("haeftlinge_marsch"),
+                             h4("Juristische Aufarbeitung"),
+                             textOutput("aufarbeitung_marsch"),
+                             h4("Verlauf/Orte"),
+                             textOutput("verlauf_marsch"))),
+                  hidden(div(id="transport", 
+                             h4(1)))
+                ),
                 mainPanel(leafletOutput("map", height="85vh"),
                           dateRangeInput("time", NULL, start="1945-01-01", end="1945-05-08", language="de", weekstart=1, width="500px"),
                           checkboxInput("osrm", "OSRM", FALSE),
+                          checkboxInput("cond", "Conditional Test", FALSE),
                           actionButton("center", "Center"))
   )
 )
@@ -60,9 +106,24 @@ saxony_geojson <- geojson_read("C:\\Users\\Fabian\\Documents\\data\\saxony.geojs
 server <- function(input, output, session) {
   
   output$map <- renderLeaflet(leaflet() %>% 
-                              addTiles() %>%
-                              setView(lng=13.5, lat=50.95, zoom=8) %>%
-                              addGeoJSON(saxony_geojson, color="blue", fill=FALSE))
+                                addTiles() %>%
+                                setView(lng=13.5, lat=50.95, zoom=8) %>%
+                                addGeoJSON(saxony_geojson, color="blue", fill=FALSE))
+  
+  getColor <- function(id) {
+    shinyjs::useShinyjs()
+    name <- csv[id, "Name"]
+    if(name == "Aussenlager KZ Flossenbuerg") {
+      color <- "orange"
+    } else if(name == "Aussenlager KZ Gross-Rosen") {
+      color <- "purple"
+    } else if(name == "Aussenlager KZ Buchenwald") {
+      color <- "green"
+    } else {
+      color <- "red"
+    }
+    return(color)
+  }
   
   addRoute <- function(id, color, weight) {
     lat <- as.numeric(data[[id]]$lat[[1]])
@@ -93,39 +154,76 @@ server <- function(input, output, session) {
   }
   
   for(i in 1:length(data)) {
-    addRoute(i, polyline_color, polyline_width)
+    addRoute(i, getColor(i), polyline_width)
   }
   
   selectRoute <- function(id) {
     if(prev) {
       leafletProxy('map') %>% clearGroup(group=as.character(prev))
-      addRoute(prev, polyline_color, polyline_width)
+      addRoute(prev, getColor(prev), polyline_width)
     }
     prev <<- id
     leafletProxy('map') %>% clearGroup(group=as.character(id))
     addRoute(id, selected_polyline_color, selected_polyline_width)
-    output$name <- renderText({csv$Name[id]})
-    output$ort <- renderText({csv$Ort[id]})
-    output$betreiber <- renderText({csv$Betreiber[id]})
-    output$dauer <- renderText({csv$Dauer.des.Bestehens[id]})
-    output$belegung <- renderText({csv$H.ftlingsbelegung[id]})
-    output$unterbringung <- renderText({csv$Unterbringung[id]})
-    output$arbeiten <- renderText({csv$Art.der.Arbeiten[id]})
-    output$todesopfer <- renderText({csv$Todesopfer[id]})
-    output$rueckueberstellungen <- renderText({csv$R.ck.berstellungen[id]})
-    output$fluchten <- renderText({csv$Fluchten[id]})
-    output$zugaenge <- renderText({csv$Zug.nge.aus.anderen.Lagern[id]})
+    if(type == "Aussenlager") {
+      shinyjs::hide(id="marsch")
+      shinyjs::hide(id="transport")
+      shinyjs::show(id="aussenlager")
+      output$name <- renderText({csv$Name[id]})
+      output$ort <- renderText({csv$Ort[id]})
+      output$standort <- renderText({csv$Standort.des.Lagers[id]})
+      output$betreiber <- renderText({csv$Betreiber[id]})
+      output$dauer <- renderText({csv$Dauer.des.Bestehens[id]})
+      output$belegung <- renderText({csv$H.ftlingsbelegung[id]})
+      output$unterbringung <- renderText({csv$Unterbringung[id]})
+      output$arbeiten <- renderText({csv$Art.der.Arbeiten[id]})
+      output$todesopfer <- renderText({csv$Todesopfer[id]})
+      output$rueckueberstellungen <- renderText({csv$R.ck.berstellungen[id]})
+      output$fluchten <- renderText({csv$Fluchten[id]})
+      output$zugaenge <- renderText({csv$Zug.nge.aus.anderen.Lagern[id]})
+      output$evakuierung <- renderText({csv$Evakuierung[id]})
+      output$aufarbeitung <- renderText({csv$Juristische.Aufarbeitung[id]})
+      output$evakuierung_besonderheiten <- renderText({csv$Besonderheiten.der.Evakuierung[id]})
+      output$lager_besonderheiten <- renderText({csv$Besonderheiten.des.Lagers[id]})
+      output$evakuierung_ende <- renderText({csv$Ende.der.Evakuierung[id]})
+      output$vorkommnisse <- renderText({csv$Todesopfer.Vorkommnisse[id]})
+      output$verlauf <- renderText({csv$Verlauf.Orte[id]})
+    } else if(type == "Marsch") {
+      shinyjs::hide(id="aussenlager")
+      shinyjs::hide(id="transport")
+      shinyjs::show(id="marsch")
+      output$name <- renderText({csv$Name[id]})
+      output$ort <- renderText({csv$Ort[id]})
+      output$betreiber <- renderText({csv$Betreiber[id]})
+      output$kolonne_marsch <- renderText({csv$St.rke.der.Kolonne[id]})
+      output$evakuierung_beginn_marsch <- renderText({csv$Beginn.der.Evakuierung[id]})
+      output$sachsen_marsch <- renderText({csv$Marsch.auf.s.chsischem.Gebiet[id]})
+      output$evakuierung_weitere_marsch <- renderText({csv$weitere.Evakuierung[id]})
+      output$ende_marsch <- renderText({csv$Ende.der.Evakuierung[id]})
+      output$besonderheiten_marsch <- renderText({csv$Besonderheiten[id]})
+      output$evakuierung_ende_marsch <- renderText({csv$Ende.der.Evakuierung[id]})
+      output$vorkommnisse_marsch <- renderText({csv$Todesopfer.Vorkommnisse[id]})
+      output$haeftlinge_marsch <- renderText({csv$H.ftlingsst.rke[id]})
+      output$aufarbeitung_marsch <- renderText({csv$Juristische.Aufarbeitung[id]})
+      output$verlauf_marsch <- renderText({csv$Verlauf.Orte[id]})
+    } else if(type == "Transport") {
+      shinyjs::hide(id="aussenlager")
+      shinyjs::hide(id="marsch")
+      shinyjs::show(id="transport")
+    }
   }
   
   observeEvent(input$map_marker_click, {
     p <- input$map_marker_click
     id <- p$id
+    type <<- csv[[id, "Typ"]]
     selectRoute(id)
   })
-  
+  shinyjs::useShinyjs()
   observeEvent(input$map_shape_click, { 
     p <- input$map_shape_click
     id <- p$id
+    type <<- csv[[id, "Typ"]]
     selectRoute(id)
   })
   
@@ -133,7 +231,7 @@ server <- function(input, output, session) {
     leafletProxy('map') %>% clearMarkers() %>% clearShapes()
     for(i in 1:length(data)) {
       if(csv[[i, "Datum"]] >= input$time[1] & csv[[i, "Datum"]] <= input$time[2]) {
-        addRoute(i, polyline_color, polyline_width)
+        addRoute(i, getColor(i), polyline_width)
       }
     }
   }, ignoreInit = TRUE)
@@ -141,14 +239,14 @@ server <- function(input, output, session) {
   observeEvent(input$center, {
     leafletProxy('map') %>% setView(lng=13.5, lat=50.95, zoom=8)
   }, ignoreInit = TRUE)
-
+  
   observeEvent(input$osrm, {
     if(input$osrm) {
       osrm <<- TRUE
       leafletProxy('map') %>% clearShapes()
       for(i in 1:length(data)) {
         if(csv[[i, "Datum"]] >= input$time[1] & csv[[i, "Datum"]] <= input$time[2]) {
-          addRoute(i, polyline_color, polyline_width)
+          addRoute(i, getColor(i), polyline_width)
         }
       }
     } else {
@@ -156,7 +254,7 @@ server <- function(input, output, session) {
       leafletProxy('map') %>% clearMarkers() %>% clearShapes()
       for(i in 1:length(data)) {
         if(csv[[i, "Datum"]] >= input$time[1] & csv[[i, "Datum"]] <= input$time[2]) {
-          addRoute(i, polyline_color, polyline_width)
+          addRoute(i, getColor(i), polyline_width)
         }
       }
     }
